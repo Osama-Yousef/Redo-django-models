a767e4bc387756d8232afa6c2460207e1e44c73c
Osama-Yousef <osamawalidyousef@gmail.com>
2020-09-22 00:41:11 +0300

    finished lab27 again but without tests

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+
+from django.shortcuts import render
+from django.views.generic import  ListView , DetailView
+from .models import Post
+
+
+
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Post
+
+class BlogDetailsView(DetailView):
+    template_name = 'post_detail.html'
+    model = Post
